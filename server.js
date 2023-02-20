14820e5efda06a21b50132b48a8290659263ec4f
seangaug <seanjnr1991@aol.com>
2023-02-20 15:47:58 +0000

    new endpoint created server.js

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const {sequelize} = require("./db")
 
 const port = 3000;
 
-//TODO
+// create a new endpoint for when the browser makes a `GET` request to http://localhost:3000/musicians.
+app.get("/musicians", async (req, res) => {
+    // The endpoint will need to fetch these musicians from the database
+    const musicians = await Musician.findAll();
+    // Send the musicians as a JSON Response
+    res.json(musicians);
+})
 
 app.listen(port, () => {
     sequelize.sync();
